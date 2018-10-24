704dbb12a916d5f1c5ef039c13fd32ec59876d0d
Florent Poinsard <florent.poinsard@epitech.eu>
2018-10-24 18:57:49 +0200

    [MODIFY] - docker-compose.yml removed orgpa-website service

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
-mkdir src
-cd src
+mkdir src && cd src
 git clone git clone git@github.com:orgpa/orgpa-database-api.git
-git clone git clone git@github.com:orgpa/orgpa.git
+cd ..
+mkdir frontend && cd frontend
+git clone git@github.com:orgpa/orgpa-frontend.git
 cd ..
