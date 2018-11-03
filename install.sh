4ba73c8aff7211f86ac84a637f0ede68596027f5
Florent Poinsard <florent.poinsard@epitech.eu>
2018-11-03 16:35:01 +0100

    [ADD] - install.sh - update script

--- install.sh
+++ install.sh
@@ -1,8 +1,22 @@
 #! /bin/sh
 
+## Installing vendore tool
+go get -v github.com/kardianos/govendor
+
 mkdir src && cd src
-git clone git clone git@github.com:orgpa/orgpa-database-api.git
-cd ..
-mkdir frontend && cd frontend
+
+## Cloning repositories
+git clone git@github.com:orgpa/orgpa-database-api.git
 git clone git@github.com:orgpa/orgpa-frontend.git
+
+## Vendoring dependencies in Orgpa-Database-API
+cd orgpa-database-api
+govendor init
+govendor add +external
+cd ..
+
+## Vendoring dependencies in Orgpa-Frontend
+cd orgpa-frontend
+govendor init
+govendor add +external
 cd ..
