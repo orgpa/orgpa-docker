60d95d07dad04bac74de26c3008af874c350d554
Florent Poinsard <florent.poinsard@epitech.eu>
2018-12-19 00:29:55 +0100

    update of install script

--- install.sh
+++ install.sh
@@ -3,20 +3,23 @@
 ## Installing vendore tool
 go get -v github.com/kardianos/govendor
 
+export GOPATH=$GOPATH:$PWD
+
 mkdir src && cd src
 
-## Cloning repositories
+## Clone repositories
 git clone git@github.com:orgpa/orgpa-database-api.git
 git clone git@github.com:orgpa/orgpa-frontend.git
 
 ## Vendoring dependencies in Orgpa-Database-API
 cd orgpa-database-api
-govendor init
-govendor add +external
+govendor init && govendor add +external
 cd ..
 
 ## Vendoring dependencies in Orgpa-Frontend
 cd orgpa-frontend
-govendor init
-govendor add +external
+govendor init && govendor add +external
+cd ..
+
+## Back to orgpa-docker
 cd ..
